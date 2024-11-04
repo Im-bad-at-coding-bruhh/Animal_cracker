eeb5209fc01f0fb92f07e1af96751890c4ede02b
Mba <uyieziirmiya@gmail.com>
2024-11-04 01:21:21 +0100

    nothing

---
+++ py.py
@@ -0,0 +1,7 @@
+Animal_cracker = [('name1','name2')]
+
+for a,b in Animal_cracker:
+    if input(['name1'])[0] == input(['name2'])[0]:
+        print(f'Yeah {a} and {b} start with the same letters')
+    else:
+        print('Na...try again :)')
